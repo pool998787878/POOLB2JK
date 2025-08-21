e2e947694651050dd8ba9264bfc709f32b038d39
ApazaMamaniWilsonR <171001ronald@gmail.com>
2025-08-21 10:38:43 -0500

    soy paul

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
@@ -0,0 +1,11 @@
+package pe.edu.upeu.asistencia.control;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+<<<<<<< HEAD
+
+=======
+>>>>>>> 4e77263258ddf9e79187afcd7bb4df7a1803d2e5
+public class AsistenciaController {
+}
