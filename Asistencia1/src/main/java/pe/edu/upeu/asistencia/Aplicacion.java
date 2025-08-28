e17692f34ff62fd3820c8fe1a1505a1a56d0c423
ApazaMamaniWilsonR <171001ronald@gmail.com>
2025-08-28 11:14:18 -0500

    sdfugerayfbayre

--- Asistencia1/src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.asistencia;
-
-public class Aplicacion {
-    public static void main(String[] args) {
-        System.out.println("Iniciando asistencia DMP");
-        AsistenciaApplication.main(args);
-    }
-}
